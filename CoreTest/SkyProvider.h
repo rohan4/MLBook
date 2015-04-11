7394808650175b9a4c205f786296c1d3ab9529b5
rohan4 <rohanshah@nat-tvwna-inside-visitornet101-a-22098.princeton.org>
2015-04-11 16:54:58 -0400

    For Hack Princeton
    ebook

---
+++ CoreTest/SkyProvider.h
@@ -0,0 +1,30 @@
+//
+//  SkyProvider.h
+//  SkyEPub
+//
+//  Created by SkyTree on 14. 11. 13..
+//  Copyright (c) 2014년 SkyEpub Corporation. All rights reserved.
+//Encryption
+
+#import <Foundation/Foundation.h>
+#import "ContentProvider.h"
+
+@class SkyProvider;
+@class Book;
+
+
+@protocol SkyProviderDataSource <NSObject>
+@optional
+-(NSString*)skyProvider:(SkyProvider*)sp keyForEncryptedData:(NSString*)uuidForContent contentName:(NSString*)contentName uuidForEpub:(NSString*)uuidForEpub;
+@end
+
+@interface SkyProvider : NSObject <ContentProvider> {
+    __weak Book* book;
+    __weak id <SkyProviderDataSource> dataSource;
+}
+@property (nonatomic, weak) id <SkyProviderDataSource> dataSource;
+@property (nonatomic, weak) Book* book;
+
+-(void)destroy;
+
+@end
